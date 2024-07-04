90ae6eaf2457aeab83e8c58b2c1a37cd0cbb7ac0
jenifferlissette <jeniffer_1002@outlook.com>
2024-07-04 10:31:37 -0500

    Intent Implicito y Explicito

--- 01-clases/app/src/main/java/com/example/a2024accgr1jlip/MainActivity.kt
+++ 01-clases/app/src/main/java/com/example/a2024accgr1jlip/MainActivity.kt
@@ -1,11 +1,63 @@
 package com.example.a2024accgr1jlip
 
+import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
 import android.widget.Button
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
+    fun mostrarSnackBar(texto:String){
+        val snack = Snackbar.make(
+            findViewById(R.id.id_layout_main),
+            texto,
+            Snackbar.LENGTH_INDEFINITE
+        )
+    }
+    val callbackContenidoIntentExplicito =
+        registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ){
+            result ->
+            if (result.resultCode == Activity.RESULT_OK){
+                if (result.data != null){
+                    //Logica negocio
+                    val data = result.data;
+                    mostrarSnackBar(
+                        "${data?.getStringExtra("nombreModificado")}"
+                    )
+                }
+            }
+        }
+    val callbackContenidoIntentImplicito =
+        registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ){
+                result ->
+            if (result.resultCode == Activity.RESULT_OK){
+                if (result.data != null){
+                    //Logica negocio
+                    if(result.data!!.data != null){
+                        val uri: Uri = result.data!!.data!!
+                        val cursor = contentResolver.query(
+                            uri, null, null, null, null, null
+                        )
+                        cursor?.moveToFirst()
+                        val indiceTelefono = cursor?.getColumnIndex(
+                            ContactsContract.CommonDataKinds.Phone.NUMBER
+                        )
+                        val telefono = cursor?.getString(indiceTelefono!!)
+                        cursor?.close()
+                        mostrarSnackBar("Telefono $telefono")
+                    }
+                }
+            }
+        }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -14,10 +66,38 @@ class MainActivity : AppCompatActivity() {
             setOnClickListener{
                 irActividad(ACicloVida::class.java)
         }
+
         val botonListView = findViewById<Button>(R.id.btn_ir_list_view)
         botonListView.
             setOnClickListener{
                 irActividad(BListView::class.java)
+        }
+        val botonIntentImplicito = findViewById<Button>(
+            R.id.btn_ir_intent_implicito
+        )
+        botonIntentImplicito.setOnClickListener{
+            val intentConRespuesta = Intent(
+                Intent.ACTION_PICK,
+                ContactsContract.CommonDataKinds.Phone.CONTENT_URI
+            )
+            callbackContenidoIntentImplicito.launch(intentConRespuesta)
+        }
+        val botonIntentExplicito = findViewById<Button>(
+            R.id.btn_ir_intent_explicito
+        )
+        botonIntentExplicito.setOnClickListener{
+            val intentExplicito = Intent(
+                this,
+                CIntentExplicitoParametros::class.java
+            )
+            intentExplicito.putExtra("nombre","Jeniffer")
+            intentExplicito.putExtra("apellido","Ichau")
+            intentExplicito.putExtra("edad", 23)
+            intentExplicito.putExtra(
+                "entrenador",
+                BEntrenador(10, "Jeniffer", "Ichau")
+            )
+            callbackContenidoIntentExplicito
         }
 
     }
